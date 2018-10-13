a924f97777597fe650ec14bfd350cb638a60b48f
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2018-10-13 12:12:25 -0400

    added listen

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.route('/')
 .get((req, res, next) => {
     res.status(200).send("You've made a GET request to the root!")
 })
+
+app.listen(8080, () => {
+    console.log("Listening on port 8080")
+})
