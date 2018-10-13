7679282000aac273f33510cf144bfe68e1b0184f
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2018-10-13 12:00:09 -0400

    Added first route

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+
+app.route('/')
+.get((req, res, next) => {
+    res.status(200).send("You've made a GET request to the root!")
+})
