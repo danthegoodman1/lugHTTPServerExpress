be3acb4ecc1370ca2ec7dc60a72572dc5bf02c43
vgutta <vineethg@udel.edu>
2018-10-13 12:30:18 -0400

    /:name/:message

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.route('/')
     res.status(200).send("You've made a GET request to the root!")
 })
 
+app.get('/:name/:message', function(req, res){
+    res.send(`Hi ${req.params.name}, your message: ${req.params.message}`);
+})
+
 app.listen(8080, () => {
     console.log("Listening on port 8080")
 })
